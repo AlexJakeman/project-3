f920313c26c09caa6c39529e1d194e8883bf3b8d
AlexJakeman <Alexjakeman4@gmail.com>
2024-01-24 07:09:10 +0000

    removed unused js file

--- app/static/js/method-delete.js
+++
@@ -1,8 +0,0 @@
-function deleteTerm(id) {
-    if (confirm("Are you sure you want to delete this term?")) {
-        // Manually set the form action and submit it
-        const form = document.getElementById(`deleteForm${id}`);
-        form.action = `/delete/${id}`;
-        form.submit();
-    }
-}
